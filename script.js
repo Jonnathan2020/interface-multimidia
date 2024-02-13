f4b5806fef4d43f0ad049aff7354f17d93edbeb3
Jonnathan.code <71984956+Jonnathan2020@users.noreply.github.com>
2024-02-12 23:16:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var btradio = document.getElementById('bt-radio')
+var btblue = document.getElementById('bt-bluetooth')
+
+function radioclick(){    
+    window.alert("modo rádio conectado")
+}
+
+btradio.addEventListener('click', radioclick)
+
+function blueclick(){    
+    window.alert("conectando bluetooth")
+}
+
+btblue.addEventListener('click', blueclick)
+
+function radioclick(){    
+    window.alert("modo rádio conectado")
+}
+
+btradio.addEventListener('click', radioclick)
+
